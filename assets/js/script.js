f640be34f16bc60318cc5c4eafbf2400429a5132
brynvictoria <Bry@DESKTOP-54E6LKU>
2023-12-20 21:17:14 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const body = document.body
+
+const div = document.querySelector("div")
+const spanH1 = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+spanBye.remove()
+div.append(spanBye)
